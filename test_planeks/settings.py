580ddac85861f3589a74d274aedb4584e31ae697
artyom <art.volyk@gmail.com>
2021-01-29 13:29:02 +0200

    add region setting for S3

--- test_planeks/settings.py
+++ test_planeks/settings.py
@@ -130,7 +130,7 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_URL = 'https://{}.s3.amazonaws.com/'.format(AWS_STORAGE_BUCKET_NAME)
 AWS_S3_SIGNATURE_VERSION = 's3v4'
-
+AWS_S3_REGION_NAME = 'eu-west-3'
 MEDIA_URL = AWS_URL + '/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
